295e6c108646fad7f092131288286166e55bdc48
Andres <andres.ac.cervantes@gmail.com>
2023-07-26 00:54:22 -0700

    Split files for dependency purposes. Implemented pages and highlighted current page text color.

---
+++ websiteDriver.js
@@ -0,0 +1,46 @@
+/*
+Runs on page load to manage page resources. As of now, it simply manages the current page using page manager. 
+*/
+window.onload = pageManager()
+
+
+/*
+Handles current page highlight
+*/
+function pageManager(){
+    //newDir is the object obtained from the previous page
+    var newDir = getPressedButton();
+        
+
+    //menuButtons is a list of all anchor elements in current page. 
+    var menuButtons = document.getElementsByClassName('navbuttons')[0].getElementsByTagName('a');
+
+    console.log("My new dir is" + newDir);
+
+    for (var i = 0; i < menuButtons.length; i++) {
+        if (menuButtons[i].href === newDir) {
+            menuButtons[i].classList.add('selected')
+            break;
+        }
+    }
+}
+
+/*
+Simple function that sets the current directed_page, called in navbar page buttons
+*/
+function setPressedButton(clickedButton){
+    window.sessionStorage.setItem("directed_page", clickedButton);
+}
+
+/*
+Returns page being directed to if it exists. Returns homepage if it does not exist.
+*/
+function getPressedButton(){
+    var actionPressed = window.sessionStorage.getItem("directed_page");
+    if(actionPressed){
+        return actionPressed;
+    }
+    else{
+        return null
+    }
+}
