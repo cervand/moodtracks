66c63ae01446295aabe25f71c5557c70b388b448
Andres <andres.ac.cervantes@gmail.com>
2023-08-15 17:23:40 -0700

    Genuinely fixed the URI now

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ const code = params.get("code");
 var trackScope = 'short_term';
 var trackLimit = '50';
 var songsToShow = 10;
-//var redirect_uri = "http://www.moodtracks.me/yourmoodtracks.html"
+var redirect_uri = "http://www.moodtracks.me/yourmoodtracks.html"
 
 
 
@@ -27,7 +27,7 @@ export async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5500/yourmoodtracks.html");
+    params.append("redirect_uri", redirect_uri);
     params.append("scope", "user-read-private user-read-email user-top-read");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -62,7 +62,7 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5500/yourmoodtracks.html");
+    params.append("redirect_uri", redirect_uri);
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
