3393711b2620eef7fcd2dc75ce101a4e9c02ab47
Andres <andres.ac.cervantes@gmail.com>
2023-08-17 23:51:43 -0700

    Beginning of rare tracks.

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 const clientId = "e74c0bff1ca849d5a028a9f445c6ead3"; // Replace with your client ID
-const redirectUri = "https://www.moodtracks.me/yourmoodtracks.html";
-const apiScope = "user-read-private user-read-email user-top-read";
+const redirectUri = "http://localhost:5500/yourmoodtracks.html";
+const apiScope = "user-read-private user-read-email user-top-read user-library-read";
 
 var trackScope = 'short_term';
 const trackLimit = '50';
-const songsToShow = 10;
+const displayedTrackListLimit = 10;
+var fullUserTrackList = [];
+var loadedUserTracks = false;
 
 const urlParams = new URLSearchParams(window.location.search);
 let code = urlParams.get('code');
@@ -67,26 +69,28 @@ else {
     var mediumTermUserTracks = await getUserTopTracks('medium_term', trackLimit);
     var longTermUserTracks = await getUserTopTracks('long_term', trackLimit);
 
-    populateTopTracks(shortTermUserTracks);
+    populateTopTracks(shortTermUserTracks, "short");
+    //saveUserLibraryStartingFrom(0);
+    //console.log(fullUserTrackList);
 }
 
 var shortTermButton = document.querySelector('#top-tracks-past-month-button');
 shortTermButton.addEventListener('click', function () {
-    populateTopTracks(shortTermUserTracks);
+    populateTopTracks(shortTermUserTracks, "short");
     removeAllCurrentHighlightedButtons();
     shortTermButton.classList.add("selected-top-tracks-option");
 });
 
 var mediumTermButton = document.querySelector('#top-tracks-past-six-months-button');
 mediumTermButton.addEventListener('click', function () {
-    populateTopTracks(mediumTermUserTracks);
+    populateTopTracks(mediumTermUserTracks, "medium");
     removeAllCurrentHighlightedButtons();
     mediumTermButton.classList.add("selected-top-tracks-option");
 });
 
 var longTermButton = document.querySelector('#top-tracks-all-time-button');
 longTermButton.addEventListener('click', function () {
-    populateTopTracks(longTermUserTracks);
+    populateTopTracks(longTermUserTracks, "long");
     removeAllCurrentHighlightedButtons();
     longTermButton.classList.add("selected-top-tracks-option");
 });
@@ -136,12 +140,61 @@ async function getUserTopTracks(timePeriod, trackListLength) {
     return await response.json();
 }
 
-async function populateTopTracks(userTopTrackData) {
-    var songList = document.getElementById("songList");
-    songList.innerHTML = ""; // Clear previous entries
 
-    for (var i = 0; i < songsToShow; i++) {
-        var songName = document.createElement("span");
+async function getResponse(apiCall){
+    let accessToken = localStorage.getItem('access_token');
+
+    const response = await fetch(apiCall, {
+        headers: {
+            Authorization: 'Bearer ' + accessToken
+        }
+    });
+
+    return await response.json();
+}
+
+async function saveUserLibraryStartingFrom(offset){
+    var userLibrarySegment = await getResponse('https://api.spotify.com/v1/me/tracks?limit=50&offset='+offset);
+
+    for(var i = 0; i <= userLibrarySegment.items.length; i++){
+        fullUserTrackList.push(userLibrarySegment.items[i]);
+    }
+    console.log(userLibrarySegment.next);
+    console.log(fullUserTrackList);
+
+    if(userLibrarySegment.next != null){
+        return await saveUserLibraryStartingFrom(offset+50);
+    }
+    else{
+        loadedUserTracks=true;
+        return fullUserTrackList;
+    }
+}
+
+function sortTracksBy(attribute){
+
+}
+
+async function populateTopTracks(userTopTrackData, timePeriod) {
+    var trackList = document.getElementById("top-tracks-story-track-list");
+    trackList.innerHTML = ""; // Clear previous entries
+
+    var topTrackTitle = document.getElementById("top-tracks-story-title");
+    
+    switch(timePeriod){
+        case "short":
+            topTrackTitle.innerHTML="My top tracks of the past month";
+            break;
+        case "medium":
+            topTrackTitle.innerHTML="My top tracks of the past six months";
+            break;
+        case "long":
+            topTrackTitle.innerHTML="My top tracks of all time";
+            break;
+    }
+
+    for (var i = 0; i < displayedTrackListLimit; i++) {
+        var trackName = document.createElement("span");
         var artistName = document.createElement("span");
         var individualTrackData = document.createElement("div");
         var rankNumber = document.createElement("span");
@@ -151,13 +204,13 @@ async function populateTopTracks(userTopTrackData) {
         rankNumber.setAttribute("id", "rankNumber");
 
         rankNumber.textContent = (i + 1).toString();
-        songName.textContent = userTopTrackData.items[i].name +' - ';
+        trackName.textContent = userTopTrackData.items[i].name +' - ';
         artistName.textContent = userTopTrackData.items[i].artists[0].name;
 
-        individualTrackData.appendChild(songName);
+        individualTrackData.appendChild(trackName);
         individualTrackData.appendChild(artistName);
 
-        songList.appendChild(rankNumber)
-        songList.appendChild(individualTrackData);
+        trackList.appendChild(rankNumber)
+        trackList.appendChild(individualTrackData);
     }
 }
